f6bfa80a2410eadbeacf3076ee70e5850c19673c
Khaled <khaledenglish@yahoo.com>
2020-03-07 16:41:12 -0700

    Add files via upload

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,10 +10,16 @@ let mix = require('laravel-mix');
  | file for your application, as well as bundling up your JS files.
  |
  */
+mix.sourceMaps();
+
+// keep paths; e.g., fontawesome
+mix.options({
+    processCssUrls: false,
+});
 
 mix.js('src/js/lib.js', 'dist/js/')
     .js('src/js/app.js', 'dist/js/')
     .sass('src/scss/lib.scss', 'dist/css/')
     .sass('src/scss/app.scss', 'dist/css/')
+    .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'dist/webfonts')
     .setPublicPath('dist'); // This is solves the '95% emitting issue'
-
